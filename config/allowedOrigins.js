88c32aacf676e49cade58e62e7708852bf4520af
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-03 15:57:21 -0500

    updated allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,5 @@
 const allowedOrigins = [
-  'https://technotes-06m4.onrender.com'
+  'https://technotes-06m4.onrender.com',
+  "https://technotes-api-40ec.onrender.com"
 ]
 module.exports = allowedOrigins
