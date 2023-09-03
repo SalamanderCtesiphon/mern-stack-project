53ad8b30d7d501476f1d66932cc19272101f986a
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-03 15:40:01 -0500

    updated allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,4 @@
 const allowedOrigins = [
-  'http://localhost:3000',
-  'https://www.dandrepairshop.com',
-  'https://dandrepairshop.com'
+  'https://technotes-06m4.onrender.com'
 ]
-
 module.exports = allowedOrigins
